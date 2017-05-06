ca0c95265723002a13636da20e7033bb00045dd4
Nirjal Khadka <nirjal@gmail.com>
2017-05-06 22:56:09 +0200

    aggregation the weak relationship, ownership

---
+++ aggregation/aggregation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aggregation
+{
+    class Program
+    {
+
+        //Also referred to as the weak association
+        static void Main(string[] args)
+        {
+            Department computerScienceDept = new Department() { Name = "Computer Science"};
+            Department biologyDept = new Department() { Name = "Biology Dept" };
+
+            
+            Teacher computerTeacher = new Teacher() { Name = "Sushil"};
+            computerScienceDept.Teachers.Add(computerTeacher);
+            biologyDept.Teachers.Add(computerTeacher);
+        }
+    }
+}
