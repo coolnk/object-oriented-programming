d1dceef39e66bb5ed1607be1e99e24d57cedf77f
Nirjal Khadka <nirjal@gmail.com>
2017-05-06 23:02:30 +0200

    Association example

--- association/association/Program.cs
+++ association/association/Program.cs
@@ -8,8 +8,14 @@ namespace association
 {
     class Program
     {
+        //Association why because the student and teacher have association and can live independent of each other. The student does not own the teacher
         static void Main(string[] args)
         {
+            Student nirjal = new Student();
+            List<Teacher> teachers = new List<Teacher>();
+            nirjal.AddTeachers(teachers);           
+
+
         }
     }
 }
---
+++ association/association/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace association
+{
+    class Student
+    {
+        List<Teacher> teachers;
+        public void AddTeachers(List<Teacher> teachers)
+        {
+            this.teachers = teachers;
+        }
+    }
+}
